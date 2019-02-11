486d7a9b6dd10eb5998ae085f95d80be38d50184
kevinleclaire <kevinleclaire3@hotmail.com>
2019-02-11 09:26:35 -0500

    button widget

---
+++ lib/app/hiragana/answer_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:tp3/app/hiragana/hiragana.dart';
+
+class AnswerButton extends StatefulWidget {
+  var _possibleAnswer;
+  var isPressed = false;
+  Question _question;
+
+  AnswerButton(value, question)
+      : _possibleAnswer = value,
+        _question = question;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AnswerButtonState();
+  }
+}
+
+class _AnswerButtonState extends State<AnswerButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+        textColor: widget.isPressed ? Colors.red : Colors.black,
+        child: Text(
+          widget._question.possibleAnswers[widget._possibleAnswer],
+          textScaleFactor: 2,
+        ),
+        onPressed: () => isAnswerCorrect(),
+      ),
+    );
+  }
+
+  void isAnswerCorrect() {
+    setState(() {
+      if (widget._question.possibleAnswers[widget._possibleAnswer] ==
+          widget._question.getAnswer()) {
+        widget._question.createRandomQuestion();
+      } else {
+        widget.isPressed = true;
+      }
+    });
+  }
+}
--- lib/app/hiragana/answer_list.dart
+++ lib/app/hiragana/answer_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tp3/app/hiragana/hiragana.dart';
+import 'package:tp3/app/hiragana/answer_button.dart';
 
 class AnswerList extends StatefulWidget {
   final Question _question;
@@ -14,11 +15,15 @@ class AnswerList extends StatefulWidget {
 
 class _AnswerListState extends State<AnswerList> {
   final Question _question;
-  bool isPressed1 = false;
-  bool isPressed2 = false;
-  bool isPressed3 = false;
+  List<AnswerButton> _answerButtons;
 
-  _AnswerListState(question) : _question = question;
+  _AnswerListState(question) : _question = question {
+    _answerButtons = [
+      AnswerButton(0, _question),
+      AnswerButton(1, _question),
+      AnswerButton(2, _question)
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,55 +31,8 @@ class _AnswerListState extends State<AnswerList> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              textColor: isPressed1 ? Colors.red : Colors.black,
-              child: Text(
-                _question.possibleAnswers[0],
-                textScaleFactor: 2,
-              ),
-              onPressed: () => isAnswerCorrect(0),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              child: Text(
-                _question.possibleAnswers[1],
-                textScaleFactor: 2,
-              ),
-              onPressed: () => isAnswerCorrect(1),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              child: Text(
-                _question.possibleAnswers[2],
-                textScaleFactor: 2,
-              ),
-              onPressed: () => isAnswerCorrect(2),
-            ),
-          ),
-        ],
+        children: _answerButtons,
       ),
     );
-  }
-
-  void isAnswerCorrect(int index) {
-    setState(() {
-      if (_question.possibleAnswers[index] == _question.getAnswer()) {
-        _question.createRandomQuestion();
-      }
-      else {
-        if (index == 1) {
-          isPressed1 = true;
-        } else if(index == 2) {
-          isPressed2 = true;
-        }
-        else {
-          isPressed3 = true;
-        }
-      }
-    });
   }
 }
